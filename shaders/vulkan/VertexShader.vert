77825e62c2d42c155f63c840159aee676d75427c
Niklas Birk <nikla.birk.nb@gmail.com>
2020-03-18 14:08:00 +0100

    vulkan shader

---
+++ shaders/vulkan/VertexShader.vert
@@ -0,0 +1,12 @@
+#version 460
+
+layout(location = 0) in vec3 pos;
+layout(location = 1) in vec3 colIn;
+
+layout(location = 0) out vec3 colV;
+
+void main(void)
+{
+    colV = colIn;
+    gl_Position = vec4(pos, 1);
+}
---
+++ shaders/vulkan/runCompiler.bat
@@ -0,0 +1,3 @@
+D:\Programme\VulkanSDK\1.1.106.0\Bin\glslangValidator.exe -V ComputeShader.comp
+D:\Programme\VulkanSDK\1.1.106.0\Bin\glslangValidator.exe -V VertexShader.vert
+D:\Programme\VulkanSDK\1.1.106.0\Bin\glslangValidator.exe -V FragmentShader.frag
